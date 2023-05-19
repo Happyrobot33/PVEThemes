776bd6b67a21f1701e1bbe5249b41be8efffedcc
Happyrobot33 <matthewherber@sbcglobal.net>
2023-05-19 12:56:18 -0400

    Update websocketHandler.js

--- websocketHandler.js
+++ websocketHandler.js
@@ -3,33 +3,33 @@ var ticket;
 var requestString;
 
 async function sendShellCommand(requestStringVar) {
-    protocol = (location.protocol === 'https:') ? 'wss://' : 'ws://';
+	protocol = (location.protocol === 'https:') ? 'wss://' : 'ws://';
 	requestString = requestStringVar;
 
-    var params = {};
-    var url = '/nodes/' + Proxmox.NodeName;
-    API2Request({
-	method: 'POST',
-	params: params,
-	url: url + '/termproxy',
-	success: function(result) {
-	    var port = encodeURIComponent(result.data.port);
-	    ticket = result.data.ticket;
-        //https://aether.matthewherber.com:8007/api2/json/nodes/Aether/termproxy/vncwebsocket?port=8007&vncticket=
-	    socketURL = protocol + location.hostname + ((location.port) ? (':' + location.port) : '') + '/api2/json' + url + '/vncwebsocket?port=' + port + '&vncticket=' + encodeURIComponent(ticket);
+	var params = {};
+	var url = '/nodes/' + Proxmox.NodeName;
+	return new Promise((resolve, reject) => {
+		API2Request({
+			method: 'POST',
+			params: params,
+			url: url + '/termproxy',
+			success: function (result) {
+				var port = encodeURIComponent(result.data.port);
+				ticket = result.data.ticket;
+				//https://aether.matthewherber.com:8007/api2/json/nodes/Aether/termproxy/vncwebsocket?port=8007&vncticket=
+				socketURL = protocol + location.hostname + ((location.port) ? (':' + location.port) : '') + '/api2/json' + url + '/vncwebsocket?port=' + port + '&vncticket=' + encodeURIComponent(ticket);
 
-	    socket = new WebSocket(socketURL, 'binary');
-	    socket.binaryType = 'arraybuffer';
-	    socket.onopen = handleSocket;
-		//return when the socket is closed
-		socket.onclose = function() {
-			return;
-		}
-	},
-	failure: function(msg) {
-	    console.log(msg);
-	}
-    });
+				socket = new WebSocket(socketURL, 'binary');
+				socket.binaryType = 'arraybuffer';
+				socket.onopen = handleSocket;
+				socket.onclose = resolve;
+				socket.onerror = reject;
+			},
+			failure: function (msg) {
+				console.log(msg);
+			}
+		});
+	});
 }
 
 function handleSocket() {
@@ -37,7 +37,7 @@ function handleSocket() {
 	socket.send(Proxmox.UserName + ":" + ticket + "\n");
 
 	//we need to wait for the console to be ready
-	socket.onmessage = function(event) {
+	socket.onmessage = function (event) {
 		var answer = new Uint8Array(event.data);
 		//OK check
 		if (answer[0] === 79 && answer[1] === 75) {
@@ -56,75 +56,75 @@ function handleSocket() {
 
 
 function API2Request(reqOpts) {
-    var me = this;
+	var me = this;
 
-    reqOpts.method = reqOpts.method || 'GET';
+	reqOpts.method = reqOpts.method || 'GET';
 
-    var xhr = new XMLHttpRequest();
+	var xhr = new XMLHttpRequest();
 
-    xhr.onload = function() {
-	var scope = reqOpts.scope || this;
-	var result;
-	var errmsg;
+	xhr.onload = function () {
+		var scope = reqOpts.scope || this;
+		var result;
+		var errmsg;
 
-	if (xhr.readyState === 4) {
-	    var ctype = xhr.getResponseHeader('Content-Type');
-	    if (xhr.status === 200) {
-		if (ctype.match(/application\/json;/)) {
-		    result = JSON.parse(xhr.responseText);
+		if (xhr.readyState === 4) {
+			var ctype = xhr.getResponseHeader('Content-Type');
+			if (xhr.status === 200) {
+				if (ctype.match(/application\/json;/)) {
+					result = JSON.parse(xhr.responseText);
+				} else {
+					errmsg = 'got unexpected content type ' + ctype;
+				}
+			} else {
+				errmsg = 'Error ' + xhr.status + ': ' + xhr.statusText;
+			}
 		} else {
-		    errmsg = 'got unexpected content type ' + ctype;
+			errmsg = 'Connection error - server offline?';
 		}
-	    } else {
-		errmsg = 'Error ' + xhr.status + ': ' + xhr.statusText;
-	    }
+
+		if (errmsg !== undefined) {
+			if (reqOpts.failure) {
+				reqOpts.failure.call(scope, errmsg);
+			}
+		} else {
+			if (reqOpts.success) {
+				reqOpts.success.call(scope, result);
+			}
+		}
+		if (reqOpts.callback) {
+			reqOpts.callback.call(scope, errmsg === undefined);
+		}
+	}
+
+	var data = urlEncode(reqOpts.params || {});
+
+	if (reqOpts.method === 'GET') {
+		xhr.open(reqOpts.method, "/api2/json" + reqOpts.url + '?' + data);
 	} else {
-	    errmsg = 'Connection error - server offline?';
+		xhr.open(reqOpts.method, "/api2/json" + reqOpts.url);
 	}
-
-	if (errmsg !== undefined) {
-	    if (reqOpts.failure) {
-		reqOpts.failure.call(scope, errmsg);
-	    }
+	xhr.setRequestHeader('Cache-Control', 'no-cache');
+	if (reqOpts.method === 'POST' || reqOpts.method === 'PUT') {
+		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		xhr.setRequestHeader('CSRFPreventionToken', Proxmox.CSRFPreventionToken);
+		xhr.send(data);
+	} else if (reqOpts.method === 'GET') {
+		xhr.send();
 	} else {
-	    if (reqOpts.success) {
-		reqOpts.success.call(scope, result);
-	    }
+		throw "unknown method";
 	}
-	if (reqOpts.callback) {
-	    reqOpts.callback.call(scope, errmsg === undefined);
-	}
-    }
-
-    var data = urlEncode(reqOpts.params || {});
-
-    if (reqOpts.method === 'GET') {
-	xhr.open(reqOpts.method, "/api2/json" + reqOpts.url + '?' + data);
-    } else {
-	xhr.open(reqOpts.method, "/api2/json" + reqOpts.url);
-    }
-    xhr.setRequestHeader('Cache-Control', 'no-cache');
-    if (reqOpts.method === 'POST' || reqOpts.method === 'PUT') {
-	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	xhr.setRequestHeader('CSRFPreventionToken', Proxmox.CSRFPreventionToken);
-	xhr.send(data);
-    } else if (reqOpts.method === 'GET') {
-	xhr.send();
-    } else {
-	throw "unknown method";
-    }
 }
 
 function urlEncode(object) {
-    var i,value, params = [];
+	var i, value, params = [];
 
-    for (i in object) {
-	if (object.hasOwnProperty(i)) {
-	    value = object[i];
-	    if (value === undefined) value = '';
-	    params.push(encodeURIComponent(i) + '=' + encodeURIComponent(String(value)));
+	for (i in object) {
+		if (object.hasOwnProperty(i)) {
+			value = object[i];
+			if (value === undefined) value = '';
+			params.push(encodeURIComponent(i) + '=' + encodeURIComponent(String(value)));
+		}
 	}
-    }
 
-    return params.join('&');
+	return params.join('&');
 }

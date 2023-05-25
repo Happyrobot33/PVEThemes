eb0a27c9832ec46c226a36ab9acdbc5b5d9d3cd0
Happyrobot33 <matthewherber@sbcglobal.net>
2023-05-25 12:53:44 -0400

    Update PVEThemes.py

--- PVEThemes.py
+++ PVEThemes.py
@@ -233,7 +233,7 @@ def addZFSBar():
     #add the item right under the memory bar item
     items = items[:memoryBarEnd + 2] + item + items[memoryBarEnd + 2:]
 
-    define = define[:itemLineStart + 8] + items + define[itemLineStart + 8:]
+    define = define[:itemLineStart] + items + define[itemLineStart + 8:]
 
     fileContents = fileContents.replace(fileContents[defineLineStart:defineLineEnd], define)
 
